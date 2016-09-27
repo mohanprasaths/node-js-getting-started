616658ace411b249287b5d5d4d0645470599957f
mohanprasaths <mohanprasath@live.com>
2016-09-27 12:00:17 +0530

    nodejs

---
+++ main.js
@@ -0,0 +1,54 @@
+var http = require("http");
+var events =  require("events");
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var count = 0;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var names=[]
+var registerEventHandler =  function(name,mailid,password){
+	console.log(name)
+}
+
+var eventEmitter = new events.EventEmitter();
+eventEmitter.on("registerEvent",registerEventHandler);
+
+
+app.get('/',function(req,res){
+	res.send("helloworld")
+})
+
+app.get("/registeruser",function(req,res){
+	count++;
+	console.log("in node js")
+	var response = {
+		status : count
+	}
+
+	res.send(JSON.stringify(response))
+})
+
+app.post("/getData",function(req,res){
+
+	names.push(req.body.name)
+	console.log(names)
+	res.send("hi")
+
+})
+
+app.get('/getAllNames',function(req,res){
+	var allNames = {
+		names : names
+	}
+	res.send(allNames)
+})
+ 
+ var port = Number(process.env.PORT || 8081)
+
+var server = app.listen(port,function(){
+	var host = server.address().address
+	var port = server.address().port
+})
